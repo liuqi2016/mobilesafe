2b6f71748038c8dc786380775386d30a0afc6efd
liuqi2016 <1551255297@qq.com>
2016-10-25 11:00:26 +0800

    My commit project

---
+++ app/src/main/java/event/study/liuqi/mobilesafe/view/FocusTextView.java
@@ -0,0 +1,27 @@
+package event.study.liuqi.mobilesafe.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by liuqi on 2016/10/25.
+ */
+
+public class FocusTextView extends TextView {
+    public FocusTextView(Context context) {
+        super(context);
+    }
+
+    public FocusTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public FocusTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
